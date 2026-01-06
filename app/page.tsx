ac952a4e6d371c8ffcc1ac8d9c1974c1db65ca84
mmm-maymyatmon <maymyat.dev@gmail.com>
2026-01-06 09:50:19 +0700

    chore: initial Next.js + Tailwind setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className="min-h-screen bg-[#F0F7FF] overflow-x-hidden">
+      NavBar
+    </main>
+  )
+}
+
+export default Home
