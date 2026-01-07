aef710072f01e34083b6abaa0cc1a9de16113420
mmm-maymyatmon <maymyat.dev@gmail.com>
2026-01-07 15:28:39 +0700

    feat: metadata og:image

--- components/ui/ScrollDown.tsx
+++ components/ui/ScrollDown.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ScrollDown = () => {
   return (
-    <div className="absolute bottom-16 left-1/2 flex -translate-x-1/2 flex-col items-center gap-2 transition-opacity hover:opacity-80">
+    <div className="absolute sm:bottom-16 bottom-10 left-1/2 z-20 flex -translate-x-1/2 flex-col items-center gap-2">
       <span className="font-medium text-[18px] leading-[1.3] tracking-[-0.02em]">Scroll to learn more</span>
       <Image src="/images/hero/arrow-down.svg" alt="" width={30} height={30} />
     </div>
